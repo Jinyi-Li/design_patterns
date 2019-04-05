84ddbd92a8f7cc63159aae5e6297b55e0c9ae23f
Jinyi-Li <li.jinyi@outlook.com>
2019-04-04 21:30:11 -0400

    [add] add singleton code

--- Singleton/LoggerDriver.java
+++ Singleton/LoggerDriver.java
@@ -1,34 +1,13 @@
-/*
-    TemperatureObserver is a "Observer" (the "Subscriber") in Observer Pattern.
+/**
+ * LoggerDriver demonstrates the use case and context of Singleton Pattern.
+ */
+public class LoggerDriver {
 
-    It provides methods to update its own state, for example, a temperatureGrade value.
-*/
-public class TemperatureObserver implements Observer {
-    
-    private static int observerIdTracker = 0;
+    public static void main(String[] args) {
+        Logger loggerForTask1 = Logger.getInstance();
+        System.out.println(loggerForTask1.hashCode());
 
-    private int observerId;
-    private Subject alarmSubject;
-    private double temperatureGrade;
-
-    
-    public TemperatureObserver(Subject alarmSubject) {
-        this.alarmSubject = alarmSubject;
-        observerId = ++observerIdTracker;
-
-        alarmSubject.register(this);
-    }
-
-
-    /*
-        It receives whole information about each update and subtract only necessary info that matters to itself, for example, a temperatureGrade value.
-    */
-    @Override
-    public void update(double temperatureGrade, double windPowerGrade, double humidityGrade) {
-        this.temperatureGrade = temperatureGrade;
-    }
-
-    public void unregister(){
-        alarmSubject.unregister(this);
+        Logger loggerForTask2 = Logger.getInstance();
+        System.out.println(loggerForTask2.hashCode());
     }
 }
