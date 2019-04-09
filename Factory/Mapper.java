33d4bccf209a9955f4cc7d91137a26bb659a7be6
Jinyi-Li <li.jinyi@outlook.com>
2019-04-09 14:27:41 -0400

    [update] update abstract factory demo

---
+++ Factory/Mapper.java
@@ -0,0 +1,3 @@
+public interface Mapper {
+    void parseFile();
+}
--- Factory/MapperFactory.java
+++ Factory/MapperFactory.java
@@ -2,7 +2,11 @@
     
 */
 public class MapperFactory {
-    public static Mapper getMapper(){
-        return new 
+    public static Mapper getMapper(String name){
+        switch(name){
+            case "Daily_Activity": return new DailyMapper();
+            case "Monthly_Activity": return new MonthlyMapper();
+            default: throw new IllegalArgumentException("Illegal type of mapper");
+        }
     }
 }
