dbd1abcbb990a9d6efd4bdca53902beeb7197c7f
Jinyi-Li <li.jinyi@outlook.com>
2019-04-26 22:15:30 -0400

    [add] add builder demo

--- Builder/Recipe.java
+++ Builder/Recipe.java
@@ -1,25 +1,56 @@
+/*
+ * The class Recipe customizes instances via its static nested class RecipeBuilder.
+ */
 public class Recipe {
-	private double onion;
-	private double tomato;
-	private double potato;
-	private double garlic;
-	private double pear;
-	private double lemon;
-
-	private boolean salt;
-	private boolean blackPepper;
-	private boolean whitePepper;
-	private boolean soy;
-	
-	private String description;
-
-	private Recipe(){
-		// just initialize an instance which is only accessible inside class Recipe.		
-	}
 
 	public static class RecipeBuilder {
-		
+		private double onion;
+		private boolean salt;
+		private boolean pepper;
+		private String description;
 
+		private RecipeBuilder() {		
+		}
+
+		public RecipeBuilder addOnion(double theOnion) {
+			onion = theOnion;
+			return this;
+		}
+
+		public RecipeBuilder withSalt() {
+			salt = true;
+			return this;
+		}
+
+		public RecipeBuilder withPepper() {
+			pepper = true;
+			return this;
+		}
+
+		public RecipeBuilder addDescription(String theDesc) {
+			description = theDesc;
+			return this;
+		}
 		
+		public Recipe build() {
+			return new Recipe(this);
+		}
 	}
+
+	public static RecipeBuilder builder() {
+		return new RecipeBuilder();
+	}
+
+	private double onion;
+	private boolean salt;
+	private boolean pepper;
+	private String description;
+
+	private Recipe(RecipeBuilder builder) {
+		onion = builder.onion;
+		salt = builder.salt;
+		pepper = builder.pepper;
+		description = builder.description;
+	}
+
 }
